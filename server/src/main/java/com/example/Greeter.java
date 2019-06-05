c25c8e0bf0052566296f3212e64b57fb94052761
Dapinder Singh <dapsingh@microsoft.com>
2019-06-06 01:35:36 +0530

    fix for spaces

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,10 @@ public class Greeter {
 
   }
 
-/**
-*@param someone
-*@return String 
-*/
+  /**
+   * @param someone
+   * @return String
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
